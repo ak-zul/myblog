8cd7602c6ca4cbdeb280f6928f172834374921a2
ak-zul <ak.zul65@gmail.com>
2016-10-01 17:16:06 +0530

    persist user data without photo

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1 class="page-header">Create Page</h1>
+
+        {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
+
+            <div class="form-group">
+                {!! Form::label('name','name') !!}
+                {!! Form::text('name',null,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('email','Email') !!}
+                {!! Form::email('email',null,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id','Role') !!}
+                {!! Form::select('role_id',array(''=>'-- Select option --') +$roles,null,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('is_active','status') !!}
+                {!! Form::select('is_active',array(1=>'Active',0=>'Not Active'),0,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('file','File:') !!}
+                {!! Form::file('file',null,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password','Password:') !!}
+                {!! Form::password('password',['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('create post',['class'=>'btn btn-primary']) !!}
+            </div>
+
+
+
+        {!! Form::close() !!}
+
+    @include('includes.form_error')
+@endsection
